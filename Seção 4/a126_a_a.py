fc3d948454bea5759abe25005f5c68465f0e1265
Ikaro <ikaromm@hotmail.com>
2023-09-25 16:56:51 -0300

    a

---
+++ Seção 4/a126_a_a.py
@@ -0,0 +1,56 @@
+# Sets - Conjuntos em Python (tipo set)
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+# Criando um set
+# set(iterável) ou {1, 2, 3}
+# s1 = set('Luiz')
+
+s1 = set()  # vazio
+s1 = {'Luiz', 1, 2, 3}  # com dados
+
+# l1 = [1, 2, 3, 3, 3, 3, 3, 1]
+# s1 = set(l1)
+# l2 = list(s1)
+# s1 = {1, 2, 3}
+# print(3 not in s1)
+# for numero in s1:
+#     print(numero)
+
+
+# Sets são eficientes para remover valores duplicados
+# de iteráveis.
+# - Não aceitam valores mutáveis;
+# - Seus valores serão sempre únicos;
+# - não tem índexes;
+# - não garantem ordem;
+# - são iteráveis (for, in, not in)
+
+# Métodos úteis:
+# add, update, clear, discard
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
+
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s3 = s1 & s2
+s3 = s2 - s1
+s3 = s1 ^ s2
+print(s3)
+
+# Exemplo de uso dos sets
+# letras = set()
+# while True:
+#     letra = input('Digite: ')
+#     letras.add(letra.lower())
+
+#     if 'l' in letras:
+#         print('PARABÉNS')
+#         break
+
+#     print(letras)
