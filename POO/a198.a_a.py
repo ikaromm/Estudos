4241350ebd25edc34b7498d2813354a0fdcab720
Ikaromm <ikaromm@hotmail.com>
2023-10-03 11:47:29 -0300

    Initial POO

---
+++ POO/a198.a_a.py
@@ -0,0 +1,134 @@
+# class - Classes são moldes para criar novos objetos
+# As classes geram novos objetos (instâncias) que
+# podem ter seus próprios atributos e métodos.
+# Os objetos gerados pela classe podem usar seus dados
+# internos para realizar várias ações.
+# Por convenção, usamos PascalCase para nomes de
+# classes.
+# string = 'Luiz'  # str
+# print(string.upper())
+# print(isinstance(string, str))
+
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+
+p1 = Pessoa('Luiz', 'Otávio')
+
+p2 = Pessoa('Maria', 'Joana')
+
+print(p1.nome)
+print(p1.sobrenome)
+
+print(p2.nome)
+print(p2.sobrenome)
+
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+
+string = 'Luiz'
+print(string.upper())
+
+fusca = Carro('Fusca')
+print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro(nome='Celta')
+print(celta.nome)
+celta.acelerar()
+
+class Animal:
+    # nome = 'Leão'
+
+    def __init__(self, nome):
+        self.nome = nome
+
+        variavel = 'valor'
+        print(variavel)
+
+    def comendo(self, alimento):
+        return f'{self.nome} está comando {alimento}'
+
+    def executar(self, *args, **kwargs):
+        return self.comendo(*args, **kwargs)
+
+
+leao = Animal(nome='Leão')
+print(leao.nome)
+print(leao.executar('maçã'))
+
+class Camera:
+    def __init__(self, nome, filmando=False):
+        self.nome = nome
+        self.filmando = filmando
+
+    def filmar(self):
+        if self.filmando:
+            print(f'{self.nome} JÁ está filmando...')
+            return
+
+        print(f'{self.nome} está filmando...')
+        self.filmando = True
+
+    def parar_filmar(self):
+        if not self.filmando:
+            print(f'{self.nome} NÃO está filmando...')
+            return
+
+        print(f'{self.nome} está parando de filmar...')
+        self.filmando = False
+
+    def fotografar(self):
+        if self.filmando:
+            print(f'{self.nome} não pode fotografar filmando')
+            return
+
+        print(f'{self.nome} está fotografando...')
+
+
+c1 = Camera('Canon')
+c2 = Camera('Sony')
+
+c1.filmar()
+c1.filmar()
+c1.fotografar()
+c1.parar_filmar()
+c1.fotografar()
+
+print()
+
+c2.parar_filmar()
+c2.filmar()
+c2.filmar()
+c2.fotografar()
+c2.parar_filmar()
+c2.fotografar()
+
+
+# Atributos de classe
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+
+
+p1 = Pessoa('João', 35)
+p2 = Pessoa('Helena', 12)
+
+print(Pessoa.ano_atual)
+# Pessoa.ano_atual = 1
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
