0326485edeb168dda77721f40041853458b7eefa
Ikaromm <ikaromm@hotmail.com>
2023-11-18 16:16:20 -0300

    add tcc help

--- Exercícios/ex9.py
+++ Exercícios/ex9.py
@@ -1,1 +1,18 @@
-import pandas as pd
+
+cpf = input('digite seu cpf: ')
+# cpf.strip()
+cpf = cpf.strip()
+cpf = cpf.replace('.','')
+cpf = cpf.replace('-','')
+
+print(f'{cpf}')
+
+
+if cpf.isnumeric():
+    if len(cpf) == 11:
+        print('cpf valido')
+    else:
+        print('cpf invalido')
+else:
+    print('cpf invalido')        
+
